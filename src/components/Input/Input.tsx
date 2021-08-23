2639ca6fd04e4e4042adaba177a5160e36f40ca7
pedro-candido <pedroferreirac@hotmail.com>
2021-08-22 23:23:39 -0300

    feat: input modifier

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,26 @@
 import React from 'react' 
-import { TextInput, View, Text, TextInputComponent } from 'react-native'
+import { TextInput, View, Text, TextInputComponent, StyleSheet } from 'react-native'
 
 interface InputProps extends TextInputComponent{
-    position: string
+    position: 'topRight' | 'topLeft' | 'bottomLeft' | 'bottomRight'
 }
 
-const Input = ({ position }: InputProps) => <View>
-    <Text>{ position }</Text>
-    <TextInput />
-</View>
+const Input = ({ position }: InputProps) => {
+
+    const styles = StyleSheet.create({
+        container: {
+            position: 'absolute',
+        }
+    })
+
+    return( 
+        <View>
+            <Text>{ position }</Text>
+            <TextInput />
+        </View>
+    )
+}
+
+
 
 export default Input
